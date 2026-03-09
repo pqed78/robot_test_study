845cdf73849ee8ef22bbcd44d2f99903c68bdbd0
Taek Yong Hwang <taekyong.hwang@gmail.com>
2026-03-09 11:25:04 +0900

    files in the mdp foler added for both tasks

--- isaac_so_arm101/source/isaac_so_arm101/isaac_so_arm101/tasks/manager_based/lift/__init__.py
+++ isaac_so_arm101/source/isaac_so_arm101/isaac_so_arm101/tasks/manager_based/lift/__init__.py
@@ -13,11 +13,20 @@ from . import agents
 
 
 gym.register(
-    id="Template-Isaac-So-Arm101-v0",
+    id="Isaac-SO-ARM100-Lift-Cube-v0",
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": f"{__name__}.isaac_so_arm101_env_cfg:IsaacSoArm101EnvCfg",
+        "env_cfg_entry_point": f"{__name__}.joint_pos_env_cfg:SoArm100LiftCubeEnvCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+    },
+)
+
+gym.register(
+    id="Isaac-SO-ARM100-Lift-Cube-Play-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.joint_pos_env_cfg:SoArm100LiftCubeEnvCfg_PLAY",
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
     },
 )
